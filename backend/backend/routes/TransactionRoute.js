1769ce41de0fefb461b3892ba2ac3f6395471451
Anas Azam <anasazammaher@gmail.com>
2025-01-07 04:42:57 +0530

    folder change

--- backend/backend/routes/TransactionRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getTransactions, addTransactions, deleteTransactions } = require("../controllers/TransactionController");
-const router = express.Router();
-
-router.get("/", getTransactions);
-router.post("/", addTransactions);
-router.delete("/:id", deleteTransactions);
-
-module.exports = router;
